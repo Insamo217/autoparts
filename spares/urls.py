d993cfdab919f93130c3cc134fda1dee16ef6d19
Alexei Bolibok <insamo217@yandex.ru>
2021-01-18 11:02:45 +0300

    added category list

--- spares/urls.py
+++ spares/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from .views import *
 
+
 urlpatterns = [
     path('', index, name='index_url'),
     path('category/', category),
-    path('category/<str:category_name>/', spares, name='category_url'),
-    path('category/<str:category_name>/<str:id>/', spare_card)
+    path('category/<str:category>/', categories_list),
+    path('category/<str:category>/<str:category_name>/', spares, name='category_url'),
+    path('category/<str:category>/<str:category_name>/<str:id>/', spare_card),
+
 ]
--- spares/views.py
+++ spares/views.py
@@ -8,12 +8,12 @@ def index(request):
 
 
 def category(request):
-    categories_list = Category.objects.all()
+    categories_list = ['Передняя подвеска', 'Тормозная система', 'Three']
     return render(request, 'spares/category.html',
                   context={'categories_list': categories_list})
 
 
-def spares(request, category_name):
+def spares(request, category_name, category):
     search_query = request.GET.get('search', '')
     title = category_name
     if search_query:
@@ -25,10 +25,23 @@ def spares(request, category_name):
                   context={'spares': spares, 'title': title})
 
 
-def spare_card(request, category_name, id):
+def spare_card(request, category_name, id, category):
     spares = Spares.objects.filter(id=id)
     return render(request, 'spares/spare_card.html',
                   context={'spares': spares})
+
+
+def categories_list (request, category):
+    if category=='Передняя подвеска':
+        categories_list = Category.objects.filter\
+            (id__in=[3,10,16,18,66,91,104,105,106,120,141])
+    elif category=='Тормозная система':
+        categories_list = Category.objects.filter \
+            (id__in=[4, 10, 12, 16, 24, 106, 124])
+    return render(request, 'spares/category.html',
+                  context={'categories_list': categories_list})
+
+
 
 
 
